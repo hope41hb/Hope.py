83a072ab5bd7daefa99c9f2bfefe906836383ede
hope41hb <xiyyou2435@gmail.com>
2026-03-15 23:44:17 +0800

    Update Airline_main.py the second times

--- Apache airline CLI/Airline_main.py
+++ Apache airline CLI/Airline_main.py
@@ -1,9 +1,10 @@
-from Airline_manager import AirlineManager
+from Airline_manager2 import AirlineManager2
 
+todo = AirlineManager2()
 #Main user interface for the Airline Booking System，CLI
 class main:
     #Create an instance of AirlineManager
-    todo = AirlineManager()
+
 
     #Main menu loop
     #The program runs continuously until the user chooses to exit.
@@ -22,18 +23,22 @@ class main:
 
           #Option 1: Check seat availability
           if choice == "1":
-             num = input("Please enter your seat's number:").strip().upper()
+             num = input("Please enter your seat's number(e.g. 1A, 1B....) :").strip().upper()
              print(todo.check_availability(num))
 
           #Option 2: Book a seat
           elif choice == "2":
              num = input("Please enter your seat's number:").strip().upper()
-             print(todo.book_a_seat(num))
+             passport = input("Passport number: ")
+             first = input("First name: ")
+             last = input("Last name: ")
+             print(todo.book_a_seat(num, passport, first, last))
 
           #Option 3: Free a seat
           elif choice == "3":
+              booking_reference = input("Please enter your seat's booking reference:").strip()
               num = input("Please enter the seat number to free: ").strip().upper()
-              print(todo.free_a_seat(num))
+              print(todo.free_a_seat(num, booking_reference))
 
           #Option 4: Show all booked seats
           elif choice == "4":
@@ -41,8 +46,8 @@ class main:
 
           #Option 5: Choosing drink
           elif choice == "5":
-              num = input("Which seats:").strip().upper()
-              drink = input("What drinks do you want? Coke/ Apple juice/ Orange juice/ sparkling water").strip()
+              num = input("Which seat are you in?:").strip().upper()
+              drink = input("What drinks do you want? Coke/ Apple juice/ Orange juice/ sparkling water").strip().lower()
               print(todo.drink(num,drink))
 
           #Option 6: Exit program
